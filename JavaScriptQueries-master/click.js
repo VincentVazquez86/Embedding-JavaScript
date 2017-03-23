91b3d8af4a72e2da2e6220ad63c79d0895a3b784
VincentVazquez86 <7vvazquez@stpatrick.org>
2017-03-23 10:49:45 -0500

    Add files via upload
    Started

--- JavaScriptQueries-master/click.js
+++ JavaScriptQueries-master/click.js
@@ -1,3 +1,15 @@
 //declare samecolor() here
+function samecolor(){
+    const doc = document;
+    let color = "009900";
 
+    let divs = doc.querySelectorAll("#samecolor div");
+
+    divs{0}.style.background=black
+    divs{0}.style.height= "500px"
+    divs{1}.style.opacity = 0.75;
+}
 //declare diffcolor() here
+function dffcolor(){
+    let color = "9933s  00";
+}
